345121b7e438bdf89835ff0795a50f4a1ef2188b
wiroj-tam <134731702+wiroj-tam@users.noreply.github.com>
2023-06-13 12:08:27 +0700

    Add files via upload

--- client/src/components/WidgetWrapper.jsx
+++ client/src/components/WidgetWrapper.jsx
@@ -1,9 +1,12 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
+import { useSelector } from "react-redux";
+
 
 const WidgetWrapper = styled(Box)(({ theme }) => ({
+
   padding: "1.5rem 1.5rem 0.75rem 1.5rem",
-  backgroundColor: theme.palette.background.alt,
+  backgroundColor: useSelector((state) => state.colorTheme.background.alt),
   borderRadius: "0.75rem",
 }));
 
