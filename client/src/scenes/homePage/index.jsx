0fef987a0cec77d58f08a54a75765893a7de7d49
wiroj-tam <134731702+wiroj-tam@users.noreply.github.com>
2023-06-13 12:03:18 +0700

    Add files via upload

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -11,8 +11,14 @@ const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const { _id, picturePath } = useSelector((state) => state.user);
 
+  {/* Color */}
+  const background_default = useSelector((state) => state.colorTheme.background.default);
+  const background_alt = useSelector((state) => state.colorTheme.background.alt);
+  // const medium = useSelector((state) => state.colorTheme.neutral.medium);
+  // const main = useSelector((state) => state.colorTheme.neutral.main);
+
   return (
-    <Box>
+    <Box backgroundColor={background_default}>
       <Navbar />
       <Box
         width="100%"
@@ -22,7 +28,7 @@ const HomePage = () => {
         justifyContent="space-between"
       >
         <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-          <UserWidget userId={_id} picturePath={picturePath} />
+          <UserWidget backgroundColor={background_alt} userId={_id} picturePath={picturePath} />
         </Box>
         <Box
           flexBasis={isNonMobileScreens ? "42%" : undefined}
